2c8f96fd8bef2964d89481311b1be4fc2e2b436a
pamatya <hydroweb.pranujamatya@gmail.com>
2025-07-09 23:03:20 +0200

    INTERMEDIATE: Still debugging the pixel-putting

--- src/renders.c
+++ src/renders.c
@@ -6,7 +6,7 @@
 /*   By: pamatya <pamatya@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 21:50:08 by pamatya           #+#    #+#             */
-/*   Updated: 2025/07/09 21:51:37 by pamatya          ###   ########.fr       */
+/*   Updated: 2025/07/09 22:23:39 by pamatya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void		render_overview(t_game *game, char *path_to_map);
 // static void	place_block(int hor, int vert, int block_color, int offset);
-static void	place_block(t_img *img, int *coords, int block_color, int offset);
+static void	place_block(t_img *img, int i, int j, int block_color);
 static void	draw_border(t_img *img, int	width, int height, int color);
 
 
@@ -23,7 +23,6 @@ void	render_overview(t_game *game, char *path_to_map)
 	char	*map_layer;
 	int		i;
 	int		j;
-	int		coords[2];
 	
 	game->map->fd = open(path_to_map, O_RDONLY);
 	if (game->map->fd < 0)
@@ -38,12 +37,10 @@ void	render_overview(t_game *game, char *path_to_map)
 		i = -1;
 		while (*(map_layer + ++i))
 		{
-			coords[0] = i;
-			coords[1] = j;
 			if (*(map_layer + i) == '1')
-				place_block(game->map->overview, coords, STONE_GRAY, 1);
+				place_block(game->map->overview, i, j, STONE_GRAY);
 			else if (*(map_layer + i) == '0')
-				place_block(game->map->overview, coords, 0, 1);
+				place_block(game->map->overview, i, j, 0);
 			// usleep(200);
 			// if (mlx_image_to_window(game->mlx, game->map->overview, 0, 0) < 0)
 			// 	exit_early(game, "Image to window failed", EXIT_FAILURE);
@@ -59,30 +56,17 @@ void	render_overview(t_game *game, char *path_to_map)
 /*
 Function to place a block/tile of size as defined in the HEADER as BLOCK_SIZE
   - if block color is provided as 0, then block_color will be white
-  - if offset = 0, no offset is used
-  - if offset = 1, the offset of the map wil be used i.e. MAP_OFFSET_(X, Y)
-  - any other value of offset may be later used to add functionality of the fn
-  - i and j are block numbers/counters in a tiled world representing the whole
-	block of several pixels BLOCK_SIZE in width and height
+  - x and y are the starting coordinates (from upper left corner) of blocks in a
+  	tiled world representing the whole block of several pixels BLOCK_SIZE in 
+	width and height
 */
-static void	place_block(t_img *img, int *coords, int block_color, int offset)
+static void	place_block(t_img *img, int x, int y, int block_color)
 {
 	t_game	*game;
 	int		i;
 	int		j;
-	int		off[2];
 
 	game = get_game();
-	if (offset == 1)
-	{
-		off[0] = MAP_OFFSET_X;
-		off[1] = MAP_OFFSET_Y;
-	}
-	else
-	{
-		off[0] = 0;
-		off[1] = 0;
-	}
 	if (!block_color)
 		block_color = WHITE;
 	j = -1;
@@ -90,35 +74,36 @@ static void	place_block(t_img *img, int *coords, int block_color, int offset)
 	{
 		i = -1;
 		while (++i < BLOCK_SIZE)
-			mlx_put_pixel(img, coords[0] * BLOCK_SIZE + i + off[0],
-				coords[1] * BLOCK_SIZE + j + off[1], block_color);
+			mlx_put_pixel(img, x * BLOCK_SIZE + i, y * BLOCK_SIZE + j,
+					block_color);
 	}
 }
 
 static void	draw_border(t_img *img, int	width, int height, int color)
 {
-	int coords[2];
+	int i;
+	int	j;
 	int	block_x;
 	int	block_y;
 
-	coords[0] = -1;
-	coords[1] = -1;
+	i = -1;
+	j = -1;
 	block_x = width / BLOCK_SIZE;
 	block_y = height / BLOCK_SIZE;
 	
-	while (++(coords[1]) < block_y)
+	while (++j < block_y)
 	{
-		if (coords[1] == 0 || coords[1] == block_y - 1)
+		if (j == 0 || j == (block_y - 1))
 		{
-			while (++(coords[0]) < block_x)
-				place_block(img, coords, color, 0);
+			while (++i < block_x)
+				place_block(img, i, j, color);
 		}
 		else
 		{
-			while (++(coords[0]) < block_x)
+			while (++(i) < block_x)
 			{
-				if (coords[0] == 0 || coords[0] == block_x - 1)
-					place_block(img, coords, color, 0);
+				if (i == 0 || i == (block_x - 1))
+					place_block(img, i, j, color);
 			}
 		}
 	}
--- src/start.c
+++ src/start.c
@@ -6,7 +6,7 @@
 /*   By: pamatya <pamatya@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 18:22:03 by pamatya           #+#    #+#             */
-/*   Updated: 2025/07/06 22:57:05 by pamatya          ###   ########.fr       */
+/*   Updated: 2025/07/09 22:07:28 by pamatya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ void	game_start(t_game *game, char *path_to_map)
 {
 	printf("Game started\n");
 	render_overview(game, path_to_map);
-	if (mlx_image_to_window(game->mlx, game->map->overview, 0, 0) < 0)
+	if (mlx_image_to_window(game->mlx, game->map->overview, MAP_OFFSET_X, MAP_OFFSET_Y) < 0)
 		exit_early(game, "Image to window failed", EXIT_FAILURE);
 }
